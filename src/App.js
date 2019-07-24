c1d50d334b33c77d81dd6cfce9103374640f189f
rick427 <44168932+rick427@users.noreply.github.com>
2019-07-24 11:11:42 +0100

    making requests and setting up state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsperPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+    }
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
   return (
     <div className="App">
-     
+      <h1 className="display-4">App</h1>
     </div>
   );
 }
